a5b3b4d599682d253401f9b396a125a97d8a227d
Laurens Martens <66726677+LaurensMartens@users.noreply.github.com>
2020-12-02 17:31:47 +0100

    Add files via upload

---
+++ part01-Part01_11.VariousVariables/src/main/java/VariousVariables.java
@@ -0,0 +1,10 @@
+
+public class VariousVariables {
+
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double pi = 3.141592653;
+
+        System.out.println("The value of pi is: " + pi);
+    }
+}
